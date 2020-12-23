d35694b3e92db11f106caaa4351bceabab40f591
Garima Kumari <70741733+Garima2123@users.noreply.github.com>
2020-12-23 20:33:55 +0530

    Create map.go

---
+++ map.go
@@ -0,0 +1,28 @@
+package main 
+  import "fmt"
+  func main() { 
+  
+     
+    var map_1 map[int]int
+  
+    
+    if map_1 == nil { 
+      
+        fmt.Println("True") 
+    } else { 
+      
+        fmt.Println("False") 
+    } 
+  
+    
+    map_2 := map[int]string{ 
+      
+            90: "Dog", 
+            91: "Cat", 
+            92: "Cow", 
+            93: "Bird", 
+            94: "Rabbit", 
+    } 
+      
+    fmt.Println("Map-2: ", map_2) 
+} 
