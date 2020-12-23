d850f2c69d2e1d95483a1a3b8e4c17b97be06e5d
Garima Kumari <70741733+Garima2123@users.noreply.github.com>
2020-12-23 15:56:29 +0530

    Create type casting.go

---
+++ type casting.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+    var x int = 19
+    var y int = 21
+    var mul float32
+    mul = float32(x) * float32(y)
+    fmt.Printf("Multiplication = %f\n", mul)
+}
